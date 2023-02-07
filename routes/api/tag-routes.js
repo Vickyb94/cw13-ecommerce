f6a3bc5b89c2278f16a9cbaf2de4345379df4c0e
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-02-07 11:03:54 -0400

    created a function to find one tag by id and associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,21 @@ router.get('/', (req, res) => {
   })
 });
 
+//finding one tag by id and associated product data
 router.get('/:id', (req, res) => {
+  Tag.findByPk(req.params.id, {
+    attributes: ['id', 'tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'stock', 'price']
+    }]
+  })
+  .then((tagID) => {
+    res.json(tagID);
+  })
+  .catch((err) => {
+     res.json(err);
+  })
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
